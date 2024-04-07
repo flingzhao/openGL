e3d768a5a7e44e2c58db7eb0316a4d0d67c15c77
hgg <1043320627@qq.com>
2024-04-07 10:54:58 +0800

    add basic shadow mapping example, but there is many Moire pattern

---
+++ Assets/shaders/shadow_mapping.glsl
@@ -0,0 +1,87 @@
+#type vertex
+#version 330 core
+layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec3 aNormal;
+layout (location = 2) in vec2 aTexCoords;
+
+out VS_OUT {
+    vec3 FragPos;
+    vec3 Normal;
+    vec2 TexCoords;
+    vec4 FragPosLightSpace;
+} vs_out;
+
+uniform mat4 projection;
+uniform mat4 view;
+uniform mat4 model;
+uniform mat4 lightSpaceMatrix;
+
+void main()
+{
+    vs_out.FragPos = vec3(model * vec4(aPos, 1.0));
+    // 经过model矩阵变换后 Normal可能发生改变
+    vs_out.Normal = transpose(inverse(mat3(model))) * aNormal;
+    vs_out.TexCoords = aTexCoords;
+    vs_out.FragPosLightSpace = lightSpaceMatrix * vec4(vs_out.FragPos, 1.0);
+    gl_Position = projection * view * vec4(vs_out.FragPos, 1.0);
+}
+
+#type fragment
+#version 330 core
+out vec4 FragColor;
+
+in VS_OUT {
+    vec3 FragPos;
+    vec3 Normal;
+    vec2 TexCoords;
+    vec4 FragPosLightSpace;
+} fs_in;
+
+uniform sampler2D diffuseTexture;
+uniform sampler2D shadowMap;
+
+uniform vec3 lightPos;
+uniform vec3 viewPos;
+
+float shadowCalculation(vec4 fragPosLightSpace)
+{
+    // range [-1, 1]
+    vec3 projCoords = fragPosLightSpace.xyz;
+    // range [0, 1]
+    projCoords = projCoords * 0.5 + 0.5;
+    float closestDepth = texture(shadowMap, projCoords.xy).r;
+    float currentDepth = projCoords.z;
+    float shadow = currentDepth > closestDepth ? 1.0 : 0.0;
+    return shadow;
+}
+
+void main()
+{
+    vec3 color = texture(diffuseTexture, fs_in.TexCoords).rgb;
+    vec3 normal = normalize(fs_in.Normal);
+    vec3 lightColor = vec3(1.0);
+
+    // ambient 
+    vec3 ambient = 0.15 * color;
+
+    // diffuse
+    vec3 lightDir = normalize(lightPos - fs_in.FragPos);
+    float diff = max(dot(lightDir, normal), 0.0);
+    vec3 diffuse = diff * lightColor;
+
+    // specular
+    vec3 viewDir = normalize(viewPos - fs_in.FragPos);
+    float spec = 0;
+    vec3 halfway = normalize(lightDir + viewDir);
+    spec = pow(max(dot(halfway, normal), 0.0), 64.0);
+    vec3 specular = spec * lightColor;
+
+    // calculate shadow
+    float shadow = shadowCalculation(fs_in.FragPosLightSpace);
+    // vec3 lighting = (ambient + (1.0 -shadow) * (diffuse + specular)) * color;
+    vec3 lighting = ambient + (1.0 - shadow) * (diffuse + specular) * color;
+
+    FragColor = vec4(lighting, 1.0);
+}
+
+
